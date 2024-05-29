3354c6e08f661a180668f65a0c4e847d2b850f8c
oliver1 <oliver1@MacBook-Pro-de-Mac-2.local>
2024-05-29 11:27:59 -0500

    Creando un usuario y el boton de cerrar sesión

--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -23,8 +23,17 @@ class iniciarSesionViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print("Intentando Iniciar Sesion")
             if (error != nil){
                 print("Se presento el siguiente error: \(error)")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: {(user, error) in print("Intentando crear un usuario")
+                    if error != nil {
+                        print("Se presento el siguiente error al crear el usuario: \(error)")
+                    } else {
+                        print("El usuario fue creado exitosamente")
+                        self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                    }
+                })
             }else{
                 print("Inicio de sesion exitoso")
+                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
             }
         }
     }
